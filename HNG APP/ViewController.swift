97f6f373c58fd19a94307ed0a5bd52c0c2bbedec
neatwhiskey <jbopasina@student.lautech.edu.ng>
2021-08-18 08:59:11 +0100

    /Users/jamaaldeenopasina/Downloads/HNG.png /Users/jamaaldeenopasina/Desktop/Simulator Screen Recording - iPhone 12 Pro Max - 2021-08-18 at 08.58.07.mp4

--- HNG APP/ViewController.swift
+++ HNG APP/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        let myText = text.text
+        
+        lab.text = myText
+    }
+
+    @IBOutlet var text: UITextField!
+
+    @IBOutlet var lab: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    //override func didReceiveMemoryWarning() {
+      //
+    //}
 
 
 }
+
 
